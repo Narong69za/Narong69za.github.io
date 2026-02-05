6b67897feabacf55a862d3ac7370a8588afb2044
SP-Unlocked <164581482+Narong69za@users.noreply.github.com>
2026-02-05 17:50:29 +0700

    Update nav.js

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,1 +1,21 @@
 
+document.addEventListener("DOMContentLoaded", function(){
+
+  const navHTML = `
+  <header class="nav-header">
+    <nav class="nav-master">
+      <ul class="nav-menu">
+        <li><a href="index.html">Home</a></li>
+        <li><a href="packages.html">Packages</a></li>
+        <li><a href="services.html">Services</a></li>
+        <li><a href="templates.html">Templates</a></li>
+        <li><a href="seo.html">SEO</a></li>
+        <li><a href="contact.html">Contact</a></li>
+      </ul>
+    </nav>
+  </header>
+  `;
+
+  document.body.insertAdjacentHTML("afterbegin", navHTML);
+
+});
