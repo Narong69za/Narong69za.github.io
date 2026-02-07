a09ae408d637e1891ad801477ba546839ad53954
SP-Unlocked <164581482+Narong69za@users.noreply.github.com>
2026-02-08 04:09:02 +0700

    Update ai-engine.js

--- assets/js/api/ai-engine.js
+++ assets/js/api/ai-engine.js
@@ -1,1 +1,18 @@
 
+const AI = {
+
+async createVideo(prompt){
+
+return await API.request("ai/create-video",{
+prompt:prompt
+});
+
+},
+
+async swapFace(data){
+
+return await API.request("ai/swap-face",data);
+
+}
+
+};
