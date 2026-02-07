b7ff32fc1602a9330476bd84f61eaf68c4188446
SP-Unlocked <164581482+Narong69za@users.noreply.github.com>
2026-02-08 04:09:56 +0700

    Update ui-controller.js

--- assets/js/api/ui-controller.js
+++ assets/js/api/ui-controller.js
@@ -1,1 +1,18 @@
 
+document.addEventListener("DOMContentLoaded",()=>{
+
+const btn=document.querySelector(".btn-ai-create");
+
+if(btn){
+
+btn.addEventListener("click",async()=>{
+
+const result = await AI.createVideo("viral creator");
+
+console.log(result);
+
+});
+
+}
+
+});
