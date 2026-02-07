ddfa50dc456586a145a565721185453630de955e
SP-Unlocked <164581482+Narong69za@users.noreply.github.com>
2026-02-08 04:07:40 +0700

    Update api-core.js

--- assets/js/api/api-core.js
+++ assets/js/api/api-core.js
@@ -1,1 +1,19 @@
+const API = {
 
+endpoint:"https://your-api-endpoint.com",
+
+async request(path,data){
+
+const res = await fetch(`${this.endpoint}/${path}`,{
+method:"POST",
+headers:{
+"Content-Type":"application/json"
+},
+body:JSON.stringify(data)
+});
+
+return await res.json();
+
+}
+
+};
